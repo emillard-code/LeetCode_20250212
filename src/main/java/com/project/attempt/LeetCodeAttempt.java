bca262b429b1d8f9f2aff2cb6205b80314f82b97
Emerson Millard <emillard.code@gmail.com>
2025-02-16 10:26:25 -0800

    Added comments to challenge attempt.

--- src/main/java/com/project/attempt/LeetCodeAttempt.java
+++ src/main/java/com/project/attempt/LeetCodeAttempt.java
@@ -12,16 +12,22 @@ public class LeetCodeAttempt {
 
     }
 
+    // This method returns the maximum value that fulfills the challenge specifications.
     public static int maxSumOfAPairWithEqualSumOfDigits(int nums[]) {
 
+        // The default value is -1 if no match is found.
         int max = -1;
 
         for (int i = 0; i < nums.length; i++) {
 
             for (int j = i + 1; j < nums.length; j++) {
 
+                // We do not want to compare the same indices with each other.
                 if (i == j) { continue; }
 
+                // If the sum of the digits found in both indexes is the same, check if the sum of
+                // the values at those indexes surpasses int max. If yes, then overwrite int max
+                // with the sum of the current values at both indexes.
                 int sum1 = nums[i] / 10 + nums[i] % 10;
                 int sum2 = nums[j] / 10 + nums[j] % 10;
                 if (sum1 == sum2 && max < nums[i] + nums[j]) { max = nums[i] + nums[j]; }
@@ -30,6 +36,7 @@ public class LeetCodeAttempt {
 
         }
 
+        // Return int max at the end.
         return max;
 
     }
